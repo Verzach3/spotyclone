f6a86cad3272661a7334f9239d8a0e0c8a155762
Gabriel <gabrielcorreacardenas@gmail.com>
2023-07-05 19:25:14 -0500

    added styled minutes util

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -7,6 +7,7 @@ import {Slider} from "@mantine/core";
 export function Player() {
     const currentSongVal = useAtomValue(currentSong)
     const [currentSongProgress, setCurrentSongProgress] = useState("");
+    const [currentSongProgressSeconds, setCurrentSongProgressSeconds] = useState(0);
     const [currentSongVolume, setCurrentSongVolume] = useState<number>(100)
     const [isMuted, setIsMuted] = useState(false);
     const [isPlaying, setIsPlaying] = useState(false);
@@ -15,6 +16,7 @@ export function Player() {
         if (!currentSongVal) return;
         const progressInterval = setInterval(() => {
             setCurrentSongProgress(getSongPercent(currentSongVal))
+            setCurrentSongProgressSeconds(Number(getSecondsFromPercentage(currentSongVal.duration(), Number(getSongPercent(currentSongVal))).toFixed(2)))
         }, 100)
         play()
         return () => {
@@ -57,10 +59,19 @@ export function Player() {
         currentSongVal.seek(getSecondsFromPercentage(currentSongVal.duration(), percentage))
     }
 
+    // Dios mio que he hecho T-T
+    function secondsToStyledTime(seconds: number) {
+        return (seconds / 60).toFixed(2).split(".").map((v, i) => {
+            if (i === 0) return v;
+            return Math.trunc(Number(v) * 0.6)
+        }).join(":")
+    }
+
     return (
         <>
             <h1>Duration M: {formatTime(currentSongVal?.duration() || 0)} S: {currentSongVal?.duration()}</h1>
-            <h1>Progress {currentSongProgress}</h1>
+            <h1>Progress
+                %: {currentSongProgress} SM: {secondsToStyledTime(currentSongProgressSeconds)} S: {currentSongProgressSeconds} </h1>
             <h1>Is Playing?: {isPlaying}</h1>
             <button onClick={play}>Play</button>
             <button onClick={pause}>Pause</button>
